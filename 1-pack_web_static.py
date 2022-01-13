1e6bd09ff27dbcbe62b2c4449f9536d44d5bd638
youssefboussaid <youssbsd@gmail.com>
2022-01-13 12:21:52 -1100

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,14 @@ from os.path import isdir, exists
 
 
 def do_pack():
-    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    """
+    generate tgz archive using fabric
+    """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(date)
     if isdir("versions") is False:
-        local("mkdir versions")
-    local('tar -cvzf' + file_path + ' web_static')
+        local(" mkdir versions")
+    local('tar -cvzf ' + file_path + ' web_static')
     if exists(file_path):
         return file_path
     return None
